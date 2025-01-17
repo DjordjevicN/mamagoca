d73de82d7844d2746be6ae3db3eab1595ffb3fac
Nikola <nikola.dj.87@gmail.com>
2025-01-17 12:15:34 +0100

    actions 4

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Footer from "./components/Footer";
 export default function App() {
   return (
     <div className="bg-mainBg">
-      asdasdasd
+      33333
       <Logo />
       <Navigation />
       <Hero />
