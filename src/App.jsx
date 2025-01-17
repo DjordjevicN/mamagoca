cea5607d63f4a032defbdf91df5f3dc6b3539976
Nikola <nikola.dj.87@gmail.com>
2025-01-17 18:34:19 +0100

    product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import Logo from "./components/Logo";
 import Navigation from "./components/Navigation";
-import { Route, HashRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Products from "./components/Products";
 
 export default function App() {
   return (
-    <Router>
-      <div className="bg-mainBg">
-        <Logo />
-        <Navigation />
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
-    </Router>
+    <div className="bg-mainBg">
+      {/* <Logo />
+      <Navigation /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Contact from "./Contact";
-import { useLocation } from "react-router-dom";
 
 const Navigation = () => {
-  const location = useLocation();
   const [contactOpen, setContactOpen] = React.useState(false);
 
-  const isProducts = location.pathname === "/products";
   const openContact = () => {
     setContactOpen(true);
   };
@@ -16,11 +13,9 @@ const Navigation = () => {
   return (
     <div className="md:block hidden">
       <div className="text-mainText max-w-screen-md flex justify-between mx-auto p-4 mt-16">
-        <a className="" href="#torte">
-          Torte
-        </a>
+        <a href="/">Home</a>
+        <a href="/products">Torte</a>
         <a href="/products">Kolači</a>
-        {!isProducts && <a href="#about">Šta nudimo</a>}
         <button onClick={openContact}>Kontakt</button>
       </div>
       {contactOpen && <Contact closeContact={closeContact} />}
