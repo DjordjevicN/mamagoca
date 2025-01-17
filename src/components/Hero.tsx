cbb5eebb08cf437db96cca9cccbd4688733ef30b
Nikola <nikola.dj.87@gmail.com>
2025-01-17 10:52:53 +0100

    nogoca

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <div className="relative mx-auto max-w-[600px] mt-10 min-h-[750px] flex justify-center items-center">
         <img className="absolute mx-auto z-20" src={waffle} alt="waffle" />
         <img className="absolute right-[350px] -top-10" src={triangle} alt="" />
-        <img className="absolute left-[400px]" src={gocaText} alt="" />
+        {/* <img className="absolute left-[400px]" src={gocaText} alt="" /> */}
         <div className="absolute bottom-20 min-w-[400px] right-[540px] z-30">
           <div>
             <h1 className="mainFont text-7xl text-secondaryText">
