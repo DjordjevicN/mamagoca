6cda45c6ab80e1eb687c2391bdaf8813bde086a6
Nikola <nikola.dj.87@gmail.com>
2025-01-17 20:01:36 +0100

    cleaning

---
+++ src/data/products.js
@@ -0,0 +1,11 @@
+export const PRODUCTS = [
+  {
+    id: "1",
+    name: "Product 1",
+    price: 100,
+    favorite: false,
+    category: "torte",
+    image: "image.jpg",
+    description: "Product 1 description",
+  },
+];
