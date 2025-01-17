06140625c895792ada3d4d5d3b9bb22a5128f44b
Nikola <nikola.dj.87@gmail.com>
2025-01-17 17:51:43 +0100

    responsive

--- src/components/SocialProof.tsx
+++ src/components/SocialProof.tsx
@@ -8,10 +8,10 @@ const SocialProof = () => {
       className="h-[550px] bg-center bg-no-repeat bg-cover flex items-center justify-center"
     >
       <div className="bg-white py-16 px-10 lg:px-40 text-center">
-        <h1 className=" text-mainText mainFont text-base sm:text-2xl lg:text-3xl">
+        <h1 className=" md:text-mainText text-black  mainFont text-base sm:text-2xl lg:text-3xl">
           Život ne čine velike stvari, već puno malih slatkih trenutaka.
         </h1>
-        <h1 className="text-mainText mainFont text-base sm:text-2xl lg:text-3xl">
+        <h1 className="md:text-mainText text-black mainFont text-base sm:text-2xl lg:text-3xl">
           Zato svrati do nas ili nas kontaktiraj da te zasladimo!
         </h1>
       </div>
